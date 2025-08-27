ec4b58ea8db7b82438b90df31f1907bbf9398147
colden Jeanmonod <tur26337@temple.edu>
2025-08-27 13:44:42 -0400

    a message

---
+++ random.c
@@ -0,0 +1,11 @@
+#include <stdio.lib>
+#include <stdlib.h>
+
+char rand_string(char* s, size_t size) {
+    const char characterset[] = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0; i < size; i++) {
+        s[i] = characterset[rand() % 26];
+    }
+}
+
